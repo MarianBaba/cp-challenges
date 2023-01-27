3d713105d81c6ef1f7f90460698fe0325d63a4f0
MarianBaba <marianbaba16@gmail.com>
2023-01-27 16:04:14 +0100

    + StonesOnTheTable

---
+++ src/main/java/easy/StonesOnTheTable.java
@@ -0,0 +1,23 @@
+// package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StonesOnTheTable {
+    public static void main(String... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        char[] input = br.readLine().toCharArray();
+
+        int counter = 0;
+
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] == input[i + 1]) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
