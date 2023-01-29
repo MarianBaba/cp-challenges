406cb2bc7149a31d32ace16ed6a88294a2cef91a
MarianBaba <marianbaba16@gmail.com>
2023-01-29 09:37:01 +0100

    + SoftDrinking

---
+++ src/main/java/easy/SoftDrinking.java
@@ -0,0 +1,31 @@
+package easy;
+
+import java.util.Scanner;
+
+public class SoftDrinking {
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int l = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int p = scanner.nextInt();
+        int nl = scanner.nextInt();
+        int np = scanner.nextInt();
+
+        int klnl = (k * l) / nl;
+        int cd = c * d;
+        int pnp = p / np;
+
+        int min = klnl;
+        if (cd < min) {
+            min = cd;
+        }
+        if (pnp < min) {
+            min = pnp;
+        }
+        System.out.println(min/n);
+    }
+}
