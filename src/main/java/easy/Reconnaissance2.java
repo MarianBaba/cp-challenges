a5f694b2375d27ff302c39c6a6aa2476fa96e666
MarianBaba <marianbaba16@gmail.com>
2023-02-03 22:31:42 +0100

    refactor

--- src/main/java/easy/Reconnaissance2.java
+++ src/main/java/easy/Reconnaissance2.java
@@ -1,4 +1,4 @@
-// package easy;
+package easy;
 
 import java.util.Scanner;
 
@@ -18,32 +18,37 @@ public class Reconnaissance2 {
 
         for (int i = 0; i < n; i++) {
             if (i == 0) {
-                if (Math.abs(soldiers[0] - soldiers[n - 1]) < min) {
-                    min = Math.abs(soldiers[0] - soldiers[n - 1]);
+                int abs = Math.abs(soldiers[0] - soldiers[n - 1]);
+                if (abs < min) {
+                    min = abs;
                     x = 1;
                     j = n;
                 }
             } else if (i == n - 1) {
-                if (Math.abs(soldiers[n - 1] - soldiers[0]) < min) {
-                    min = Math.abs(soldiers[n - 1] - soldiers[0]);
+                int abs = Math.abs(soldiers[n - 1] - soldiers[0]);
+                if (abs < min) {
+                    min = abs;
                     x = n;
                     j = 1;
                 }
 
-                if (Math.abs(soldiers[n - 1] - soldiers[n - 2]) < min) {
-                    min = Math.abs(soldiers[n - 1] - soldiers[n - 2]);
+                int abs2 = Math.abs(soldiers[n - 1] - soldiers[n - 2]);
+                if (abs2 < min) {
+                    min = abs2;
                     x = n;
                     j = n - 1;
                 }
             } else {
-                if (Math.abs(soldiers[i] - soldiers[i + 1]) < min) {
-                    min = Math.abs(soldiers[i] - soldiers[i + 1]);
+                int abs = Math.abs(soldiers[i] - soldiers[i + 1]);
+                if (abs < min) {
+                    min = abs;
                     x = i + 1;
                     j = i + 2;
                 }
 
-                if (Math.abs(soldiers[i] - soldiers[i - 1]) < min) {
-                    min = Math.abs(soldiers[i] - soldiers[i - 1]);
+                int abs2 = Math.abs(soldiers[i] - soldiers[i - 1]);
+                if (abs2 < min) {
+                    min = abs2;
                     x = i + 1;
                     j = i;
                 }
